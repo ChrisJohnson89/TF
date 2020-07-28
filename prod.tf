4a788feb391f315185f5177257c6484dbcfb1b84
Chris Johnson <cjengineer89@gmail.com>
2020-07-28 16:56:00 +0700

    added vpc and sec groups

--- prod.tf
+++ prod.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIARN2N7FC4Q74YSLGY"
-  secret_key = "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"
+  access_key = "AKIAVDK4TRDA45NEXALF"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "tf_course" {
@@ -11,4 +11,32 @@ resource "aws_s3_bucket" "tf_course" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+   ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"] 
+   }
+   ingress {
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"] 
+   }
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"    
+     cidr_blocks = ["0.0.0.0/0"] 
+   }
+
+   tags = { 
+     "Terraform" : "true"
+   }
 }
